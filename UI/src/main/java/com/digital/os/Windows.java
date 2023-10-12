fa888602b6c1d2c59a7af24e201f12e1fa1eafc2
Zhyldyz Zheenbaeva <zhyldyzzheenbaeva@MacBook-Air-Zhyldyz.local>
2023-10-12 10:15:23 +0600

    winda

---
+++ UI/src/main/java/com/digital/os/Windows.java
@@ -0,0 +1,9 @@
+package com.digital.os;
+
+import com.sun.imageio.plugins.jpeg.JPEGStreamMetadataFormat;
+
+public class Windows {
+    public static void main(String[] args) {
+        System.out.println("jk");
+    }
+}
